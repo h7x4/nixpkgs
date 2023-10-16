3d5d963bac7f3259af38a5378716e9c12ca4468a
h7x4 <h7x4@nani.wtf>
2023-10-16 20:55:58 +0200

    nixos/wireshark: clean up module

--- nixos/modules/programs/wireshark.nix
+++ nixos/modules/programs/wireshark.nix
@@ -1,38 +1,24 @@
 { config, lib, pkgs, ... }:
-
-with lib;
-
 let
+  inherit (lib) mdDoc;
   cfg = config.programs.wireshark;
-  wireshark = cfg.package;
-in {
+in
+{
   options = {
     programs.wireshark = {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = lib.mdDoc ''
-          Whether to add Wireshark to the global environment and configure a
-          setcap wrapper for 'dumpcap' for users in the 'wireshark' group.
-        '';
-      };
-      package = mkOption {
-        type = types.package;
-        default = pkgs.wireshark-cli;
-        defaultText = literalExpression "pkgs.wireshark-cli";
-        description = lib.mdDoc ''
-          Which Wireshark package to install in the global environment.
-        '';
+      enable = lib.mkEnableOption (mdDoc "wireshark, and configure a setcap wrapper for 'dumpcap' for users in the 'wireshark' group";
+      package = lib.mkPackageOptionMD pkgs "wireshark-cli" {
+        example = lib.literalExpression "pkgs.wireshark";
       };
     };
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = [ wireshark ];
-    users.groups.wireshark = {};
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+    users.groups.wireshark = { };
 
     security.wrappers.dumpcap = {
-      source = "${wireshark}/bin/dumpcap";
+      source = "${cfg.package}/bin/dumpcap";
       capabilities = "cap_net_raw,cap_net_admin+eip";
       owner = "root";
       group = "wireshark";
