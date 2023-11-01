ee3f92b28f475156f657f0560dc5e21e1278241c
h7x4 <h7x4@nani.wtf>
2023-11-01 18:06:54 +0100

    WIP

--- nixos/modules/services/blockchain/ethereum/lighthouse.nix
+++ nixos/modules/services/blockchain/ethereum/lighthouse.nix
@@ -16,7 +16,7 @@ in {
             enable = lib.mkEnableOption (lib.mdDoc "Lightouse Beacon node");
 
             dataDir = mkOption {
-              type = types.str;
+              type = types.path;
               default = "/var/lib/lighthouse-beacon";
               description = lib.mdDoc ''
                 Directory where data will be stored. Each chain will be stored under it's own specific subdirectory.
@@ -122,12 +122,11 @@ in {
             };
 
             extraArgs = mkOption {
-              type = types.str;
+              type = with types; coercedTo (listOf str) lib.escapeShellArgs str;
               description = lib.mdDoc ''
                 Additional arguments passed to the lighthouse beacon command.
               '';
-              default = "";
-              example = "";
+              default = [ ];
             };
           };
         };
@@ -145,7 +144,7 @@ in {
             };
 
             dataDir = mkOption {
-              type = types.str;
+              type = types.path;
               default = "/var/lib/lighthouse-validator";
               description = lib.mdDoc ''
                 Directory where data will be stored. Each chain will be stored under it's own specific subdirectory.
@@ -180,12 +179,11 @@ in {
             };
 
             extraArgs = mkOption {
-              type = types.str;
+              type = with types; coercedTo (listOf str) lib.escapeShellArgs str;
               description = lib.mdDoc ''
                 Additional arguments passed to the lighthouse validator command.
               '';
-              default = "";
-              example = "";
+              default = [ ];
             };
           };
         };
@@ -200,49 +198,59 @@ in {
       };
 
       extraArgs = mkOption {
-        type = types.str;
+        type = with types; coercedTo (listOf str) lib.escapeShellArgs str;
         description = lib.mdDoc ''
           Additional arguments passed to every lighthouse command.
         '';
-        default = "";
-        example = "";
+        default = [ ];
       };
     };
   };
 
   config = mkIf (cfg.beacon.enable || cfg.validator.enable) {
 
-    environment.systemPackages = [ pkgs.lighthouse ] ;
+    environment.systemPackages = [ pkgs.lighthouse ];
 
-    networking.firewall = mkIf cfg.beacon.enable {
-      allowedTCPPorts = mkIf cfg.beacon.openFirewall [ cfg.beacon.port ];
-      allowedUDPPorts = mkIf cfg.beacon.openFirewall [ cfg.beacon.port ];
+    networking.firewall = mkIf (cfg.beacon.enable && cfg.beacon.openFirewall) {
+      allowedTCPPorts = [ cfg.beacon.port ];
+      allowedUDPPorts = [ cfg.beacon.port ];
     };
 
+    systemd.tmpfiles.rules = [ ]
+      ++ optional cfg.beacon.enable "d ${cfg.beacon.dataDir}/${cfg.network} 0700 root root - -"
+      ++ optional cfg.validator.enable "d ${cfg.validator.dataDir}/${cfg.network} 0700 root root - -";
 
-    systemd.services.lighthouse-beacon = mkIf cfg.beacon.enable {
+    systemd.services.lighthouse-beacon = mkIf  {
       description = "Lighthouse beacon node (connect to P2P nodes and verify blocks)";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
 
-      script = ''
-        # make sure the chain data directory is created on first run
-        mkdir -p ${cfg.beacon.dataDir}/${cfg.network}
-
-        ${pkgs.lighthouse}/bin/lighthouse beacon_node \
-          --disable-upnp \
-          ${lib.optionalString cfg.beacon.disableDepositContractSync "--disable-deposit-contract-sync"} \
-          --port ${toString cfg.beacon.port} \
-          --listen-address ${cfg.beacon.address} \
-          --network ${cfg.network} \
-          --datadir ${cfg.beacon.dataDir}/${cfg.network} \
-          --execution-endpoint http://${cfg.beacon.execution.address}:${toString cfg.beacon.execution.port} \
-          --execution-jwt ''${CREDENTIALS_DIRECTORY}/LIGHTHOUSE_JWT \
-          ${lib.optionalString cfg.beacon.http.enable '' --http --http-address ${cfg.beacon.http.address} --http-port ${toString cfg.beacon.http.port}''} \
-          ${lib.optionalString cfg.beacon.metrics.enable '' --metrics --metrics-address ${cfg.beacon.metrics.address} --metrics-port ${toString cfg.beacon.metrics.port}''} \
-          ${cfg.extraArgs} ${cfg.beacon.extraArgs}
-      '';
       serviceConfig = {
+        ExecStart = let
+          args = lib.cli.toGNUCommandLineShell { } lib.mkMerge [
+            {
+              disable-upnp = true;
+              disable-deposit-contract-sync = cfg.beacon.disableDepositContractSync;
+              port = cfg.beacon.port;
+              listen-address = cfg.beacon.address;
+              network = cfg.network;
+              datadir = "${cfg.beacon.dataDir}/${cfg.network}";
+              execution-endpoint = "http://${cfg.beacon.execution.address}:${toString cfg.beacon.execution.port}";
+              execution-jwt = "%d/LIGHTHOUSE_JWT";
+            }
+            (lib.mkIf (cfg.beacon.http.enable) {
+              http = true;
+              http-address = cfg.beacon.http.address;
+              http-port = cfg.beacon.http.port;
+            })
+            (lib.mkIf (cfg.beacon.metrics.enable) {
+              metrics = true;
+              metrics-address = cfg.beacon.metrics.address;
+              metrics-port = cfg.beacon.metrics.port;
+            })
+          ];
+        in "${pkgs.lighthouse}/bin/lighthouse beacon_node ${args} ${cfg.extraArgs} ${cfg.beacon.extraArgs}";
+
         LoadCredential = "LIGHTHOUSE_JWT:${cfg.beacon.execution.jwtPath}";
         DynamicUser = true;
         Restart = "on-failure";
@@ -273,19 +281,22 @@ in {
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
 
-      script = ''
-        # make sure the chain data directory is created on first run
-        mkdir -p ${cfg.validator.dataDir}/${cfg.network}
-
-        ${pkgs.lighthouse}/bin/lighthouse validator_client \
-          --network ${cfg.network} \
-          --beacon-nodes ${lib.concatStringsSep "," cfg.validator.beaconNodes} \
-          --datadir ${cfg.validator.dataDir}/${cfg.network} \
-          ${optionalString cfg.validator.metrics.enable ''--metrics --metrics-address ${cfg.validator.metrics.address} --metrics-port ${toString cfg.validator.metrics.port}''} \
-          ${cfg.extraArgs} ${cfg.validator.extraArgs}
-      '';
-
       serviceConfig = {
+        ExecStart = let
+          args = lib.cli.toGNUCommandLineShell { } lib.mkMerge [
+            {
+              network = cfg.network;
+              beacon-nodes = lib.concatStringsSep "," cfg.validator.beaconNodes;
+              datadir = "${cfg.validator.dataDir}/${cfg.network}";
+            }
+            (lib.mkIf (cfg.validator.metrics.enable) {
+              metrics = true;
+              metrics-address = cfg.validator.metrics.address;
+              metrics-port = cfg.validator.metrics.port;
+            })
+          ];
+        in "${pkgs.lighthouse}/bin/lighthouse validator_client ${args} ${cfg.extraArgs} ${cfg.validator.extraArgs}";
+
         Restart = "on-failure";
         StateDirectory = "lighthouse-validator";
         ReadWritePaths = [ cfg.validator.dataDir ];
