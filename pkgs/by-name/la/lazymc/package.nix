60b8c9eabd34e9685f400512570c476a91bb629f
Daniel Olsen <daniel.olsen99@gmail.com>
2023-11-15 00:29:11 +0100

    wrap lmao

--- pkgs/by-name/la/lazymc/package.nix
+++ pkgs/by-name/la/lazymc/package.nix
@@ -1,36 +1,68 @@
-{
-  lib
+{ lib
 , rustPlatform
 , fetchFromGitHub
 , nix-update-script
+
+, writeShellScriptBin
+
+, formats
+
+, minecraft-server
+, lazymcConfig ? { }
 }:
 
-rustPlatform.buildRustPackage rec {
-  pname = "lazymc";
-  version = "0.2.10";
+let
+  unwrapped = rustPlatform.buildRustPackage rec {
+    pname = "lazymc";
+    version = "0.2.10";
 
-  src = fetchFromGitHub {
-    owner = "timvisee";
-    repo = "lazymc";
-    rev = "v${version}";
-    hash = "sha256-IObLjxuMJDjZ3M6M1DaPvmoRqAydbLKdpTQ3Vs+B9Oo=";
+    src = fetchFromGitHub {
+      owner = "timvisee";
+      repo = "lazymc";
+      rev = "v${version}";
+      hash = "sha256-IObLjxuMJDjZ3M6M1DaPvmoRqAydbLKdpTQ3Vs+B9Oo=";
+    };
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "minecraft-protocol-0.1.0" = "sha256-vSFS1yVxTBSpx/ZhzA3EjcZyOWHbmoGARl0eMn1fJ+4=";
+      };
+    };
+
+    passthru.updateScript = nix-update-script { };
+
+    meta = with lib; {
+      description = "Remote wake-up daemon for minecraft servers";
+      homepage = "https://github.com/timvisee/lazymc";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [ h7x4 dandellion ];
+      platforms = platforms.unix;
+      mainProgram = "lazymc";
+    };  
   };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "minecraft-protocol-0.1.0" = "sha256-vSFS1yVxTBSpx/ZhzA3EjcZyOWHbmoGARl0eMn1fJ+4=";
+  startCommand = writeShellScriptBin "lazymc-launch" ''
+    trap 'kill -TERM $PID' TERM INT
+    ${minecraft-server} &
+
+    PID=$!
+    wait $PID
+    trap - TERM INT
+    wait $PID
+  '';
+
+  defaultConfig = {
+    server = {
+      directory = "/var/lib/minecraft";
+      command = startCommand;
     };
   };
 
-  passthru.updateScript = nix-update-script { };
+  mergedConfig = lib.attrsets.recursiveUpdate defaultConfig lazymcConfig;
 
-  meta = with lib; {
-    description = "Remote wake-up daemon for minecraft servers";
-    homepage = "https://github.com/timvisee/lazymc";
-    license = licenses.gpl3Only;
-    maintainers = with maintainers; [ h7x4 dandellion ];
-    platforms = platforms.unix;
-    mainProgram = "lazymc";
-  };
-}
+  configFile = (formats.toml { }).generate "lazymc-config.toml" mergedConfig;
+  
+in writeShellScriptBin "minecraft-server" ''
+  exec ${lib.getExe unwrapped} -c ${configFile}
+''
