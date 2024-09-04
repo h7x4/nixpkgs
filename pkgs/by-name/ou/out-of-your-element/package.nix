e052a43af1e5eef627cf234aba562af1640f953e
h7x4 <h7x4@nani.wtf>
2024-09-04 17:16:23 +0200

    fix

--- pkgs/by-name/ou/out-of-your-element/package.nix
+++ pkgs/by-name/ou/out-of-your-element/package.nix
@@ -1,55 +1,40 @@
 { lib
+, nodejs
 , buildNpmPackage
 , fetchNpmDeps
 , fetchFromGitea
-, git
-, cacert
+, makeWrapper
 }:
-# fetchNpmDeps {
-#   name = "out-of-your-element-deps";
-#   src = fetchFromGitea {
-#     domain = "gitdab.com";
-#     owner = "cadence";
-#     repo = "out-of-your-element";
-#     rev = "v1.3";
-#     hash = "sha256-dLDm3xOMoprkGU7D9IFX1hjMfCR3YjVgXb3c5IEuUTQ";
-#   };
-
-#   patches = [ ./add-discord-markdown-dep-integrity.patch ];
-
-  # npmDepsHash = "sha256-k2MeejRWteIzY1Q+iNLCLDLnE1z9+cBaAGYAIlQHa/4=";
-# };
 buildNpmPackage rec {
   pname = "out-of-your-element";
-  version = "1.3";
+  version = "2.3";
   src = fetchFromGitea {
     domain = "gitdab.com";
     owner = "cadence";
     repo = "out-of-your-element";
     rev = "v${version}";
-    hash = "sha256-dLDm3xOMoprkGU7D9IFX1hjMfCR3YjVgXb3c5IEuUTQ";
+    hash = "sha256-zKJAgbCiHRPeuGFo7vcJeNJYGyOde/dqALzd8W3L2bU=";
   };
 
-  patches = [ ./add-discord-markdown-dep-integrity.patch ];
-
-  # makeCacheWritable = true;
-
-  # outputHashAlgo = "sha256";
-  # outputHashMode = "recursive";
-  # outputHash = "";
-
-  # npmBuildScript = "build:prod";
+  dontPatchELF = true;
   dontNpmBuild = true;
-  # forceGitDeps = true;
-  npmDepsHash = "sha256-fPTFyX2GYSdMeIzVKXBacZxhTTj8fvlMFEGOxN0/cm8=";
+  npmDepsHash = "sha256-RtE1P/qTYyvb1setxOd2N5efPF0X0GIkVoaLgLxPuk8=";
 
-  # nativeBuildInputs = [ git cacert ];
-  # nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  # postInstall = ''
-  #   makeWrapper ${electron}/bin/electron $out/bin/uivonim \
-  #     --add-flags $out/lib/node_modules/uivonim/build/main/main.js
-  # '';
+  postInstall = ''
+    startjs="$out/lib/node_modules/out-of-your-element/start.js"
+    echo "$(echo '#!${lib.getExe nodejs}'; cat "$startjs")" > "$startjs"
+    chmod +x "$startjs"
+    makeWrapper "$startjs" "$out/bin/ooye"
+
+    for script in "$out"/lib/node_modules/out-of-your-element/scripts/*.js; do
+      echo "$(echo '#!${lib.getExe nodejs}'; cat "$script")" > "$script"
+      chmod +x "$script"
+      basename=$(basename "$script")
+      makeWrapper "$script" "$out/bin/ooye-''${basename%.*}"
+    done
+  '';
 
   meta = with lib; {
     homepage = "https://gitdab.com/cadence/out-of-your-element";
@@ -57,6 +42,6 @@ buildNpmPackage rec {
     changelog = "https://gitdab.com/cadence/out-of-your-element/releases/tag/${src.rev}";
     license = licenses.agpl3Plus;
     maintainers = with maintainers; [ h7x4 dandellion ];
-    mainProgram = "start.js";
+    mainProgram = "ooye";
   };
 }
