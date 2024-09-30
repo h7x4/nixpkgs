447b8311b01736a53e5cfd6a503aa50d56d92311
h7x4 <h7x4@nani.wtf>
2024-09-30 14:26:59 +0200

    ruby-modules/testing: lib.filterAttrs -> lib.getAttrs

--- pkgs/development/ruby-modules/testing/stubs.nix
+++ pkgs/development/ruby-modules/testing/stubs.nix
@@ -3,7 +3,7 @@ let
   mkDerivation = {name, ...}@argSet:
   derivation {
     inherit name;
-    text = (builtins.toJSON (lib.filterAttrs ( n: v: builtins.any (x: x == n) ["name" "system"]) argSet));
+    text = builtins.toJSON (lib.getAttrs ["name" "system"] argSet);
     builder = stdenv.shell;
     args = [ "-c" "echo  $(<$textPath) > $out"];
     system = stdenv.hostPlatform.system;
