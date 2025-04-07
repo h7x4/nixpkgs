63564b1a7c9d4bd87ec6be5810e78396f885f4f9
h7x4 <h7x4@nani.wtf>
2025-04-07 10:53:10 +0200

    lib/tests: concatLists map -> concatMap

--- lib/tests/check-eval.nix
+++ lib/tests/check-eval.nix
@@ -5,6 +5,6 @@ let
     "misc"
     "systems"
   ];
-  all = builtins.concatLists (map (f: import (./. + "/${f}.nix")) tests);
+  all = builtins.concatMap (f: import (./. + "/${f}.nix")) tests;
 in
 if all == [ ] then null else throw (builtins.toJSON all)
