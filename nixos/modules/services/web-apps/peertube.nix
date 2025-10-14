3bd9e5b3e74c5c265c7a8b4fde84727d0cd25036
h7x4 <h7x4@nani.wtf>
2025-10-15 00:17:31 +0900

    nixos/peertube: enable `proxyWebsockets` for relevant nginx config

--- nixos/modules/services/web-apps/peertube.nix
+++ nixos/modules/services/web-apps/peertube.nix
@@ -711,12 +711,10 @@ in
 
         locations."@api_websocket" = {
           proxyPass = "http://peertube";
+          proxyWebsockets = true;
           priority = 1240;
 
           extraConfig = ''
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection 'upgrade';
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
