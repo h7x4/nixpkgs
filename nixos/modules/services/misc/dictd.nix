6503fd6975d9f7c0505f680bbc2038a972bf5fa0
h7x4 <h7x4@nani.wtf>
2025-11-03 17:28:28 +0900

    nixos/dictd: harden service

--- nixos/modules/services/misc/dictd.nix
+++ nixos/modules/services/misc/dictd.nix
@@ -35,36 +35,103 @@ in
     };
   };
 
-  config =
-    let
-      dictdb = pkgs.dictDBCollector {
-        dictlist = map (x: {
-          name = x.name;
-          filename = x;
-        }) cfg.DBs;
+  config = lib.mkIf cfg.enable {
+    # get the command line client on system path to make some use of the service
+    environment.systemPackages = [ cfg.package ];
+
+    environment.etc."dict.conf".text = ''
+      server localhost
+    '';
+
+    systemd.services."dictd" = {
+      description = "DICT.org Dictionary Server";
+      documentation = [ "man:dictd(8)" ];
+
+      after = [ "network.target" ];
+      requires = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      environment = {
+        LOCALE_ARCHIVE = "/run/current-system/sw/lib/locale/locale-archive";
       };
-    in
-    lib.mkIf cfg.enable {
 
-      # get the command line client on system path to make some use of the service
-      environment.systemPackages = [ cfg.package ];
+      serviceConfig = {
+        Type = "forking";
+        ExecStart = let
+          dictdb = pkgs.dictDBCollector {
+            dictlist = map (x: {
+              name = x.name;
+              filename = x;
+            }) cfg.DBs;
+          };
 
-      environment.etc."dict.conf".text = ''
-        server localhost
-      '';
+          args = lib.cli.toCommandLineShellGNU { } {
+            syslog = true;
+            config = "${dictdb}/share/dictd/dictd.conf";
+            locale = "en_US.UTF-8";
+            pid-file = "/run/dictd/dictd.pid";
+          };
+        in "${lib.getExe' cfg.package "dictd"} ${args}";
 
-      systemd.services.dictd = {
-        description = "DICT.org Dictionary Server";
-        wantedBy = [ "multi-user.target" ];
-        environment = {
-          LOCALE_ARCHIVE = "/run/current-system/sw/lib/locale/locale-archive";
-        };
         # Work around the fact that dictd doesn't handle SIGTERM; it terminates
         # with code 143 instead of exiting with code 0.
-        serviceConfig.SuccessExitStatus = [ 143 ];
-        serviceConfig.Type = "forking";
-        serviceConfig.DynamicUser = true;
-        script = "${cfg.package}/sbin/dictd -s -c ${dictdb}/share/dictd/dictd.conf --locale en_US.UTF-8";
+        SuccessExitStatus = [ 143 ];
+
+        DynamicUser = true;
+
+        RuntimeDirectory = [ "dictd" "dictd/root-mnt" ];
+        RuntimeDirectoryMode = "0700";
+
+        RootDirectory = "/run/dictd/root-mnt";
+        BindPaths = [
+          # Needs to talk with DNS resolver
+          "/run"
+          "/var/run"
+        ];
+        BindReadOnlyPaths = [
+          builtins.storeDir
+          # Needs to read config of DNS resolver, as well as timezones.
+          "/etc"
+        ];
+        NoExecPaths = "/";
+        ExecPaths = "${lib.getExe' cfg.package "dictd"}";
+
+        AmbientCapabilities = "";
+        CapabilityBoundingSet = "";
+        DevicePolicy = "closed";
+        LimitNPROC = "1";
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateTmp = "disconnected";
+        PrivateUsers = true;
+        ProcSubset = "pid";
+        ProtectClock = true;
+        ProtectControlGroups = "strict";
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+          "AF_UNIX" # In order to talk to syslog socket
+        ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "@system-service"
+          "~@privileged"
+          "~@resources"
+        ];
+        UMask = "0777";
       };
     };
+  };
 }
